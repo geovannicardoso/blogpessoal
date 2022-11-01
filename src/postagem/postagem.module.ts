acf48173e78154ab0baab878c430189cdaed5f9f
geovanni.cardoso <geovanni.moreira@gmail.com>
2022-11-01 15:47:54 -0300

    implementando tem service e controller.

--- src/postagem/postagem.module.ts
+++ src/postagem/postagem.module.ts
@@ -2,7 +2,7 @@ import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { PostagemController } from "./controllers/postagem.controller";
 import { Postagem } from "./entities/postagem.entity";
-import { PostagemService } from "./service/postagem.service";
+import { PostagemService } from "./services/postagem.service";
 
 @Module({
     imports: [TypeOrmModule.forFeature([Postagem])],
