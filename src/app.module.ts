3861586081c6fc4252bde71cdb931f2661b3508a
geovanni.cardoso <geovanni.moreira@gmail.com>
2022-12-12 11:33:52 -0300

    auth module application

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
 import { Postagem } from './postagem/entities/postagem.entity';
 import { PostagemModule } from './postagem/postagem.module';
-import {Tema} from './tema/entities/tema.entity';
+import { Tema } from './tema/entities/tema.entity';
 import { TemaModule } from './tema/tema.module';
+
 @Module({
   imports: [
     TypeOrmModule.forRoot({
@@ -17,7 +19,8 @@ import { TemaModule } from './tema/tema.module';
       synchronize: true,
     }),
     PostagemModule,
-    TemaModule
+    TemaModule,
+    AuthModule
   ],
   controllers: [],
   providers: [],
