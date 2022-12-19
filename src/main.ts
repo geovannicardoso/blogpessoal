182059427fa17479c9ff273a9a12debaccb16089
geovanni.cardoso <geovanni.moreira@gmail.com>
2022-12-19 12:34:42 -0300

    Deploy

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,26 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder } from '@nestjs/swagger';
+import { SwaggerModule } from '@nestjs/swagger/dist';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  process.env.TZ = '-03:00'
+  const config = new DocumentBuilder()
+  .setTitle('Blog Pessoal')
+  .setDescription('Projeto Blog Pessoal')
+  .setContact("Generation Brasil","http://www.generationbrasil.online","generation.com")
+  .setVersion('1.0')
+  .addBearerAuth()
+  .build()
+  const document = SwaggerModule.createDocument(app,config);
+  SwaggerModule.setup('/swagger',app,document);
+
+  process.env.TZ = '-03:00';
 
   app.useGlobalPipes(new ValidationPipe());
-  app.enableCors()
-  await app.listen(4000);
+  app.enableCors();
+  await app.listen(process.env.PORT || 4000);
 }
 bootstrap();
--- src/tema/entities/tema.entity.ts
+++ src/tema/entities/tema.entity.ts
@@ -1,17 +1,22 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsNotEmpty } from "class-validator";
-import { Postagem } from "src/postagem/entities/postagem.entity";
 import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import { Postagem } from "../../postagem/entities/postagem.entity";
 
 @Entity({name: "tb_temas"})
 export class Tema {
 
-    @PrimaryGeneratedColumn()    
+    @PrimaryGeneratedColumn() 
+    @ApiProperty()   
     id: number
 
     @IsNotEmpty()
     @Column({length: 255, nullable: false})
+    @ApiProperty()
     descricao: string
-    
-    @OneToMany (() => Postagem, (postagem) => postagem.tema)
+
+    @ApiProperty()
+    @OneToMany(() => Postagem, (postagem) => postagem.tema)
     postagem: Postagem[]
+    
 }
